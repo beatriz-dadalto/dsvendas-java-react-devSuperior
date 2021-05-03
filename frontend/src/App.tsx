40580027059d7c0e43cf86c01d94a5ca804f43aa
Bia Coelho <biacoelho@pm.me>
2021-05-03 12:42:42 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import DataTable from 'assets/components/DataTable';
+import Footer from 'assets/components/Footer';
+import NavBar from 'assets/components/NavBar/indext';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Bia Coelho</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Bia Coelho</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/assets/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{' '}
+          <a
+            href="https://github.com/biacoelho"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Bia Coelho
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{' '}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
