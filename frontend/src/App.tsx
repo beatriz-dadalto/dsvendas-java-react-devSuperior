07a563f70cf75496b7a842084dcb6518990cb1dc
Bia Coelho <biacoelho@pm.me>
2021-05-03 13:22:07 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'assets/components/BarChart';
 import DataTable from 'assets/components/DataTable';
+import DonutChart from 'assets/components/DonutChart';
 import Footer from 'assets/components/Footer';
 import NavBar from 'assets/components/NavBar/indext';
 
@@ -7,7 +9,23 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Bia Coelho</h1>
+        <h1 className="text-primary py-5">Dashboard de Vendas</h1>
+
+        <div className="row px-3 py-2">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Participações nas vendas
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-4">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
--- frontend/src/assets/components/DataTable/index.tsx
+++ frontend/src/assets/components/DataTable/index.tsx
@@ -1,6 +1,6 @@
 const DataTable = () => {
   return (
-    <div className="table-responsive">
+    <div className="table-responsive py-4">
       <table className="table table-striped table-sm">
         <thead>
           <tr>
