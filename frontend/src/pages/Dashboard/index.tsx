89bb3cd93039b3e659ba47856efeae02ff853a85
Bia Coelho <biacoelho@pm.me>
2021-05-07 13:14:11 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import BarChart from "assets/components/BarChart";
+import DataTable from "assets/components/DataTable";
+import DonutChart from "assets/components/DonutChart";
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/NavBar/indext";
+
+const Dashboard = () => {
+  return (
+<>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-5">Dashboard de Vendas</h1>
+
+        <div className="row px-3 py-2">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">
+              Participações nas vendas
+            </h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-4">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home bia</h1>
+    </div>
+  )
+}
+
+export default Home;
